bf769f30eb5505996a3db9c3e7a85684fa67578f
karthikbeepi <karthikbeepi@gmail.com>
2020-06-02 17:05:25 -0500

    Delete Node LL

---
+++ C#/DeleteNodeLL.cs
@@ -0,0 +1,13 @@
+using System;
+   public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
+
+public class DeleteNodeLL {
+    public void DeleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
